d3e8f1743b4408454fe83e56684800ca721f4473
Tim Nguyen <33167258+timnguyen90@users.noreply.github.com>
2020-04-11 10:21:03 +0700

    Added Copy right
    Added Copy right

--- controller/main.js
+++ controller/main.js
@@ -1,3 +1,12 @@
+/*******************************************************
+ * Copyright (C) 2020 Tim Nguyen <mr.phucnguyen@hotmail.com>
+ * 
+ * This file is part of Ankiviet Project.
+ * 
+ * Ankiviet Project can not be copied and/or distributed without the express
+ * permission of Tim Nguyen
+ *******************************************************/
+
 $( document ).ready(function() {
     /* Initial Home Page*/
     $('#sidebar-wrapper').load('./view/Layout/sidebar/sidebar.html');
