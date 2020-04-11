4abc1245fe873820659d1c91c33c966ca8d347b3
Tim Nguyen <33167258+timnguyen90@users.noreply.github.com>
2020-04-11 10:15:08 +0700

    Fix issue: not found components
    Fix bug: not load component.

--- controller/main.js
+++ controller/main.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
     /* Initial Home Page*/
-    $('#sidebar-wrapper').load('/view/Layout/sidebar/sidebar.html');
-    $('#navigation-bar').load('/view/Layout/nav/navbar.html');
-    $('#main-content').load('/view/Layout/home/homecontent.html');
+    $('#sidebar-wrapper').load('./view/Layout/sidebar/sidebar.html');
+    $('#navigation-bar').load('./view/Layout/nav/navbar.html');
+    $('#main-content').load('./view/Layout/home/homecontent.html');
     
 });
